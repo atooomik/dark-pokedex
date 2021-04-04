fcd64f7c8274ce63856752a4b70f2851ea94ae9b
Isacc Nuñez <isacc.nunez@apithy.com>
2021-04-04 14:40:24 -0500

    hotfix baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,30 +4,30 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "dark-pokedex",
+    title: 'dark-pokedex',
     htmlAttrs: {
-      lang: "en"
+      lang: 'en'
     },
     meta: [
-      { charset: "utf-8" },
+      { charset: 'utf-8' },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
       },
-      { hid: "description", name: "description", content: "" }
+      { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    "~/assets/styles/base",
-    "~/assets/styles/components.scss",
-    "~/assets/styles/utilities"
+    '~/assets/styles/base',
+    '~/assets/styles/components.scss',
+    '~/assets/styles/utilities'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/tabler-icons", "~/plugins/vue-select"],
+  plugins: ['~/plugins/tabler-icons', '~/plugins/vue-select'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,21 +35,21 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    "@nuxtjs/eslint-module",
+    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss"
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios"
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: 'https://pokeapi.co/api/v2/'
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
-};
+}
